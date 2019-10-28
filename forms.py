3fae9ec22198cc6fa9e40def96b223e1fa1ecf5d
karobia001 <karobiamaina81@gmail.com>
2019-10-28 16:41:31 +0300

    import wtf forms that will help us in form validation

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired,Length
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
+    
+    email = StringField('Email',
+                        validators=[DataRequired(),Email()])
+    
+    password = PasField('Email')
