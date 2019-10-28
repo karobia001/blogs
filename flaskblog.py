7174372ed4008f57f73a7eccdb8c15de5f0e7ffd
karobia001 <karobiamaina81@gmail.com>
2019-10-28 12:18:56 +0300

    We input dummy data to test our templates

---
+++ flaskblog.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'denooo',
+        'title' : 'post1',
+        'content':'i am awesome',
+        'date_posted':'April20,2018'
+    },
+    {
+        'author':'maina',
+        'title' : 'post2',
+        'content':'i am awesome 2', 
+        'date_posted':'April 20,2018'   
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
