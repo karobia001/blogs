4c2f74e06e359f381e45fed42385d6575b253f9a
karobia001 <karobiamaina81@gmail.com>
2019-10-29 08:30:00 +0300

    takes the json url that will give us the quotes

--- app/request.py
+++ app/request.py
@@ -9,11 +9,4 @@ def get_quote():
         quote_details_data = url.read()
         quote_details_response = json.loads(quote_details_data)
 
-        quote_object = None
-        if quote_details_response:
-            author = quote_details_response.get('author')
-            quote = quote_details_response.get('quote')
-
-            quote_object = Quote(author,quote)
-
-    return quote_object
+        
